9ccb3c237770d81e1395183d0558fed3c41cf046
unknown <rafagonpin317@gmail.com>
2025-04-17 23:47:50 -0300

    Só falta adicionar sfx

--- components/interface.js
+++ components/interface.js
@@ -45,34 +45,34 @@ const dDiceNumber = document.querySelector("#dice-result-display") // Display
 iResType.forEach(option =>{
     option.addEventListener("input" , () => {
         resType = Number(option.value) // Muda o tipo de resultado
-        displayResInDice() // Função que exibe o resultado
+        displayResInDice(rollRes) // Função que exibe o resultado
     })
 })
 
 //Função para exibir o resultado
-function displayResInDice() {
+function displayResInDice(res , max) {
     //Exibe algo diferente de acordo com o tipo de exibição solicitado
     switch(resType) {
         // Mostrar o menor valor + modificador
         case 1:
-            dDiceNumber.innerHTML = Math.min(...rollRes) + rollModifier
+            dDiceNumber.innerHTML = Math.min(...res)
             break
         // Mostra a soma dos valores + modificador
         case 2:
             let total = 0
-            for(res of rollRes){
-                total += res
+            for(r of res){
+                total += r
             }
-            dDiceNumber.innerHTML = total + rollModifier
+            dDiceNumber.innerHTML = total
             break
         //Mostra o maior valor + modificador
         case 3:
-            dDiceNumber.innerHTML = Math.max(...rollRes) + rollModifier
+            dDiceNumber.innerHTML = Math.max(...res)
             break
     }
 
     //Caso um dos resultados seja o valor máximo do dado, adiciona um brilho extra ao display
-    if(rollRes.includes(diceType)){
+    if(res.includes(max)){
         dDiceNumber.style.setProperty("text-shadow" , "0px 0px 20px white")
     }
     else{
@@ -83,39 +83,38 @@ function displayResInDice() {
 
 
 // TROCAR O DISPLAY DE DADO
-const dDice = document.querySelector("#dice-display")
-function changeDice(diceType) {
+function changeDice(type) {
     //Retira o display de dado atual
     dDice.classList.remove("d4-display", "d6-display", "d8-display", "d10-display", "d12-display", "d20-display", "d100-display");
     //Pega o tipo de dado para alterar o display de dado
-    switch (Number(diceType.value)) {
+    switch (type) {
         //d4 (triângulo)
-        case 1:
+        case 4:
             dDice.classList.add("d4-display")
             break
         //d6 (quadrado)
-        case 2:
+        case 6:
             dDice.classList.add("d6-display")
             break
         //d8 (losângulo)
-        case 3:
+        case 8:
             dDice.classList.add("d8-display")
             break
         //d10 (hexágono assimétrico)
-        case 4:
+        case 10:
             dDice.classList.add("d10-display")
             break
         //d12 (decágono)
-        case 5:
+        case 12:
             dDice.classList.add("d12-display")
             break
-        //d20 (hexágono)
-        case 6:
-            dDice.classList.add("d20-display")
-            break
         //d100 (círculo)
-        case 7:
+        case 100:
             dDice.classList.add("d100-display")
+            break
+        //d20 (hexágono padrão)
+        default:
+            dDice.classList.add("d20-display")
             break
     }
 }
--- components/script.js
+++ components/script.js
@@ -1,124 +1,59 @@
-// CONTROLE DAS CONFIGURAÇÕES DOS DADOS (Quantidade, Tipo e Modificador) (lado Esquerdo)
-//Quantidade de dados
-let diceNum = 1 //Por padrão, a quantidade de dados é 1
-const iDiceNum = document.querySelector("#diceNumInput") //Input
-const dDiceNum = document.querySelector("#diceNumDisplay") //Display
-iDiceNum.addEventListener("input" , () => {
-    diceNum = Number(iDiceNum.value) //A variável diceNum possui a quantidade de dados escolhido pelo usuário
-    dDiceNum.innerHTML = diceNum //Display
-    
-    displayRollInfo() //Atualiza informação de rolagem
-})
-
-//Tipo de dados
-let diceType = 20; //Por padrão, o tipo de dado é o d20
-const iDiceType = document.querySelector("#diceTypeInput") //Input
-const dDiceType = document.querySelector("#diceTypeDisplay") //Display
-iDiceType.addEventListener("input" , () => {
-    //Cada valor do slider é convertido para um número de faces de um dado. A variável diceType possui o número de faces escolhido pelo usuário
-    switch (Number(iDiceType.value)) {
-        case 1:
-            diceType = 4
-            break
-        case 2:
-            diceType = 6
-            break
-        case 3:
-            diceType = 8
-            break
-        case 4:
-            diceType = 10
-            break
-        case 5:
-            diceType = 12
-            break
-        case 6:
-            diceType = 20
-            break
-        case 7:
-            diceType = 100
-            break
-    }
-    dDiceType.innerHTML = diceType //Display
-    
-    changeDice(iDiceType) //Atualiza a forma do dado
-    displayRollInfo() //Atualiza informação de rolagem
-})
-
-//Modificador da rolagem
-let rollModifier = 0 //Por padrão, o modificador é 0
-const iRollMod = document.querySelector("#rollModInput") //Input
-const dRollMod = document.querySelector("#rollModDisplay") //Display
-iRollMod.addEventListener("input" , () => {
-    rollModifier = Number(iRollMod.value) //A variável rollModifier possui o valor do modificador escolhido pelo usuário
-    //Caso o modificador seja positivo, será exibido com um "+" no início :)
-    if(rollModifier > 0){
-        dRollMod.innerHTML = "+" + rollModifier
-    }
-    else{
-        dRollMod.innerHTML = rollModifier
-    }
-
-    displayRollInfo() //Atualiza informação de rolagem
-})
-
-//Display das informações de rolagem
-const dRollInfoMod = document.querySelector("#rollInfo-Mod") //Display 
-let rollInfo //Contém a informação da rolagem
-let rollInfoMod //Informações da rolagem + modificador
-
-//Função que define a informação de rolagem
-function setRollInfo() {
-    rollInfo = diceNum + "d" + diceType
-}
-
-
-//Função que exibe a informação de rolagem
-function displayRollInfo() {
-    setRollInfo() //Define a informação
-
-    //Se o modificador for positivo, coloca um "+" :)
-    if (rollModifier > 0) {
-        rollInfoMod = rollInfo + " + " + rollModifier
-    }
-    
-    //Se o modificador for negativo, deixa ele positivo e coloca um "-" (importante deixar positivo para que o "-" padrão nn atrapalhe o estilo )
-    else if (rollModifier < 0) {
-        rollInfoMod = rollInfo + " - " + Math.abs(rollModifier)
-    }
-    
-    else {
-        rollInfoMod = rollInfo
-    }
-    
-    //Exibe a informação da rolagem no display
-    dRollInfoMod.innerHTML = rollInfoMod
-}
-
-// Botão para rolar dados
-const rollBtn = document.querySelector("#rollDices")
-rollBtn.addEventListener("click" , rollDices)
-
+const dDice = document.querySelector("#dice-display")
+const iRollQuery = document.querySelector("#rollInfo")
+const inputTemplate = /^(\d*)d(\d+)$/i
 
 
 //EXIBIÇÃO DE RESULTADOS (Tipo e Resultados) (lado Direito)
 //Rolagem de dados!!!!
-const dRollInfo = document.querySelector("#rollInfo") //Display dos dados rolados (Ex.: 1d20)
 const dRollRes = document.querySelector("#rollResults") //Display dos resultados das rolagens (Ex.: 1 3 6 10 20)
 const dicesSound = document.querySelector("#dicesSfx") //Áduio do dado rolando
-let rollRes = [0] // Resultado das rolagens
 
+
+dDice.addEventListener("click" , buildRollQuery)
+
+function buildRollQuery(){    
+    let rollQuery = (iRollQuery.value)
+    
+    let mid = rollQuery.indexOf("d")
+    
+    rollQuery = rollQuery.split("")
+    if(rollQuery.length === 0){
+        dRollRes.innerHTML = "<- Digite o valor aqui"
+        return
+    }
+    
+    let leftSide = rollQuery.slice(0 , mid)
+    leftSide = Number(leftSide.join(""))
+    leftSide <= 0 ? leftSide = 1 : false
+
+    let rightSide = rollQuery.slice(mid+1)
+    rightSide = Number(rightSide.join(""))
+
+    if(isNaN(leftSide) || isNaN(rightSide) || mid < 0){
+        dRollRes.innerHTML = "Valor Inválido!"
+        return
+    }
+
+    else if(leftSide >= 50 || rightSide >= 500){
+        dRollRes.innerHTML = "Valor muito alto!"
+        return
+    }
+
+    else if(rightSide <= 1){
+        dRollRes.innerHTML = "Valor muito baixo!"
+        return
+    }
+
+    rollDices(leftSide , rightSide)
+
+}
+
+let rollRes = [] //Reiniciando os resultados
 //FUNÇÃO QUE ROLA OS DADOS!!! (finalmente)
-function rollDices() {
+function rollDices(diceNum , diceType) {
+    rollRes = []
 
-    //SoundEffect de dado rolando
-    dicesSound.currentTime = 0
-    dicesSound.play()
-    
-    setRollInfo() //Informação dos dados rolados
-    dRollInfo.innerHTML = `${rollInfo} -->` //Exibindo os dados rolados (1d20 -->)
-    
-    rollRes = [] //Reiniciando os resultados
+
 
     //Para cada número de dados, uma rolagem é feita e o resultado é integrado no rollRes
     for(i = 1 ; i <= diceNum ; i++){
@@ -129,6 +64,6 @@ function rollDices() {
     rollRes = rollRes.sort((a,b) => a-b) //Organizando os resultados em ordem crescente
     dRollRes.innerHTML = `${rollRes.join(" ")}` //Exibindo todos os resultados, substituindo a virgula (separador padrão de array) por um espaço
 
-    displayResInDice() //Exibindo o valor no display de dado. Chama a função para exibir de acordo com o tipo de exibição que o usuario quer
-
+    displayResInDice(rollRes, diceType)
+    changeDice(diceType) //Atualiza a forma do dado
 }
